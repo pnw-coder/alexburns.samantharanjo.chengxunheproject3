923745bddbe4ed3a9c534ec56ab7a2f61e36331e
pnw-coder <133038912+pnw-coder@users.noreply.github.com>
2024-04-22 22:29:00 +0000

    Updated server file with correct paths

--- server.cjs
+++ server.cjs
@@ -2,6 +2,7 @@ const express = require('express');
 const password = require('./backend/password.api.cjs');
 const users = require('./backend/user.api.cjs')
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser')
 const path = require('path')
 
 const app = express();
@@ -17,6 +18,21 @@ const db = mongoose.connection;
 
 // This will create the connection, and throw an error if it doesn't work
 db.on('error', console.error.bind(console, 'Error connecting to MongoDB:'));
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/password', password);
+app.use('/api/users', users);
+
+let frontend_dir = path.join(__dirname, 'dist')
+
+app.use(express.static(frontend_dir));
+app.get('*', function(req, res) {
+    console.log("received request");
+    res.sendFile(path.join(frontend_dir, "index.html"));
+});
 
 app.listen(process.env.PORT || 8000, function() {
     console.log("Starting app now...")
